a4a2575f87a5a450563c567acfd697911d99a3df
Linet Oliech <linetaoliech@gmail.com>
2023-05-24 18:05:56 +0300

    configuring vite with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import About from './components/About'
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <About/>
+    </div>
+  )
+}
+
+export default App
